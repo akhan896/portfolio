970b59b1626349fed93978a14c73b549f490fc8b
Your Name <you@example.com>
2026-03-24 00:14:09 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import Certifications from './components/Certifications'
 import Achievements from './components/Achievements'
 import Contact from './components/Contact'
 import Footer from './components/Footer'
-import CursorTrail from './components/CursorTrail'
 
 function App() {
   return (
@@ -18,7 +17,6 @@ function App() {
         <span className="blob-2" />
         <span className="blob-3" />
       </div>
-      <CursorTrail />
       <Navbar />
       <main>
         <Hero />
